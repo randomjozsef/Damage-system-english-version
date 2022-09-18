0110e33fab80227505fb50029058f245becdc865
js280 <js280@RANDOM>
2022-09-18 20:49:07 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random r = new Random();
+int hp = r.Next(1, 200);
+while (true)
+{
+    Console.Write("The monster's HP " + hp + "\nHow much do you damage?: ");
+    int damage = int.Parse(Console.ReadLine());
+    hp -= damage;
+    if (hp <= 0) break;
+    else continue;
+}
+Console.WriteLine("You defeated the monster!");
+Console.ReadKey(); ;
